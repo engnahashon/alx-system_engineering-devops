a335a534357918cc39f3c3c43d6e22ba2a42f4e0
engnahashon <nahashon.ndirangu@outlook.com>
2023-04-24 09:30:17 +0000

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,5 @@
 #Automate install and conf of nginx server
-class nginx_setup {
-  exec { 'update':
-    command => '/usr/bin/apt-get -y update',
-  }
-
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  exec { 'ufw_allow_nginx':
-    command => '/usr/sbin/ufw allow "Nginx HTTP"',
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    content => "server {\n  listen 80 default_server;\n  listen [::]:80 default_server;\n  add_header X-Served-By \$HOSTNAME;\n  root /var/www/html;\n  index index.html index.htm index.nginx-debian.html;\n  server_name _;\n  location / {\n    try_files \$uri \$uri/ =404;\n  }\n}\n",
-  }
-
-  file { '/var/www/html/index.nginx-debian.html':
-    ensure  => present,
-    content => 'Hello World!',
-  }
-
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => [
-      Package['nginx'],
-      File['/etc/nginx/sites-available/default'],
-      File['/var/www/html/index.nginx-debian.html'],
-    ],
-  }
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \"\$HOSTNAME\";/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
-
-include nginx_setup
