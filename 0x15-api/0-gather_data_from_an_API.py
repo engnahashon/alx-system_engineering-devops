35d1d5c833975d7b51c10a7d40f69402ecff97d7
engnahashon <nahashon.ndirangu@outlook.com>
2023-04-24 20:57:03 +0000

    api 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+import requests
+
+employee_id = sys.argv[1]
+user_api_url = "https://jsonplaceholder.typicode.com/users/{}"\
+    .format(employee_id)
+todos_api_url = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+    .format(employee_id)
+
+response = requests.get(user_api_url)
+employee_data = response.json()
+
+response = requests.get(todos_api_url)
+employee_todos = response.json()
+
+total = len(employee_todos)
+complete = len([task for task in employee_todos if task["completed"]])
+
+print("Employee {} is done with tasks({}/{})"
+      .format(employee_data["name"], complete, total))
+for todo in employee_todos:
+    if todo["completed"]:
+        print("\t{}".format(todo["title"]))
