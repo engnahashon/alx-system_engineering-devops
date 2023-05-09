087e41d1f84fe17186a14a5ea0ea3644df6ce848
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-09 22:44:23 +0000

    recusre

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,8 +7,8 @@ def recurse(subreddit, hot_list=[], count=0, after_val=None):
     '''returns list of titles for hot articles'''
     URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     USERAGENT = {'User-Agent': 'User-Agent': 'Mozilla/5.0 (Windowsi\
-         NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 \
-        Safari/537.36'}
+         NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+        Chrome/58.0.3029.110 Safari/537.36'}
     params = {'limit': 100}
     if count > 0:
         params['after'] = after_val
