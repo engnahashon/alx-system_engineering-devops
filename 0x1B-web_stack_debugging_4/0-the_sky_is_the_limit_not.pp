117a2747b194196ef029bea06d63a706fda63932
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-18 22:30:29 +0000

    debugging #4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 #Web stack debuggin 4, change limit
 exec { 'change_limit_to_2000':
   path    => '/bin/',
-  command => 'sed -i "s/15/2000" /etc/default/nginx'
+  command => 'sed -i "s/15/2000/" /etc/default/nginx'
 }
 
 -> exec { 'restart_nginx':
