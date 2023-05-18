b185771d237fb66daddad84b461fbce8f2ab50c9
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-18 22:05:14 +0000

    debug #4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,2 +1,10 @@
 # Change file descriptor limits
+exec { 'set soft limit to 2000':
+  path    => '/bin',
+  command => "sed -i 's/4/2000/' /etc/security/limits.conf"
+}
 
+exec { 'set hard limit to 2000':
+  path    => '/bin',
+  command => "sed -i 's/5/2000/' /etc/security/limits.conf"
+}
