1b8d092c6aa0ea9f276996f7a69048a7584ac067
engnahashon <nahashon.ndirangu@outlook.com>
2023-09-07 19:47:40 +0000

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-package { 'flask':
-# Install the flask package using pip3
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-exec { 'killmenow':
-# create a manifest that kills a process named killmenow
-  command => 'pkill killmenow',
-  path    => '/usr/bin:/usr/sbin:/bin',
-}
